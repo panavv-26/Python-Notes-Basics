a2606b3bb8a6c263f3bc89654c1a92f7f65a3077
Panav Walia <panavwalia498@gmail.com>
2025-07-01 02:09:48 +0530

    Add files via upload

---
+++ from ultralytics import YOLO.py
@@ -0,0 +1,6 @@
+
+i=5
+while i >=1:
+    print("Panav Walia", i)
+    i-=1
+print("End of loop")
---
+++ loopss.py
@@ -0,0 +1,18 @@
+#while loops
+#loops are used to repeat a block of code multiple times
+
+
+#First we usually check the condition, if it is true then we execute the code inside the loop, and then we update the variable that is used in the condition.
+#The variable that is used is called TODO: iterator variable, while the process of running the loop is called FIXME:iteration.
+
+# p=1
+# while p<=5:
+#     print("Panav")
+#     p+=1
+# print(p)
+# print("Done with while loop")
+
+# p=5
+# while p>=1:
+#     print("Panav")
+#     p-=1

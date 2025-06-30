c10cf5129a461e1b68d3f82a3b7adf5d0cccb0dc
Panav Walia <panavwalia498@gmail.com>
2025-07-01 02:11:03 +0530

    Turtle.py

--- import turtle.py
+++ import turtle.py
@@ -1,18 +1,16 @@
-#while loops
-#loops are used to repeat a block of code multiple times
+import turtle
 
+t = turtle.Turtle()
+t.speed(0)
+turtle.bgcolor("black")
+colors = ["cyan", "magenta", "yellow", "white"]
 
-#First we usually check the condition, if it is true then we execute the code inside the loop, and then we update the variable that is used in the condition.
-#The variable that is used is called TODO: iterator variable, while the process of running the loop is called FIXME:iteration.
+for i in range(360):
+    t.pencolor(colors[i % 4])
+    t.circle(i, 90)
+    t.left(90)
+    t.circle(i, 90)
+    t.left(18)
 
-# p=1
-# while p<=5:
-#     print("Panav")
-#     p+=1
-# print(p)
-# print("Done with while loop")
-
-# p=5
-# while p>=1:
-#     print("Panav")
-#     p-=1
+t.hideturtle()
+turtle.done()
